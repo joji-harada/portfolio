642774891f8ad9c507e185e4d42749724e535b08
George Allison <49288165+joji-harada@users.noreply.github.com>
2019-11-14 15:45:03 -0800

    fixed viewheight

--- src/components/CenterContent/CenterContent.js
+++ src/components/CenterContent/CenterContent.js
@@ -3,12 +3,14 @@ import './CenterContent.css';
 import { SocialIcon } from 'react-social-icons';
 
 const CenterContent = props => (
-    <div className="centerContent">
-        <h1>GEORGE ALLISON</h1>
-        <ul className="centerList">
-            <li><SocialIcon url="https://github.com/joji-harada" bgColor="#000" fgColor="#fff" /></li>
-            <li><SocialIcon url="https://www.linkedin.com/in/george-allison-728975139/" bgColor="#000" fgColor="#fff" /></li>
-        </ul>
+    <div className="centerMe">
+        <div className="centerContent">
+            <h1>GEORGE ALLISON</h1>
+            <ul className="centerList">
+                <li><SocialIcon url="https://github.com/joji-harada" bgColor="#000" fgColor="#fff" /></li>
+                <li><SocialIcon url="https://www.linkedin.com/in/george-allison-728975139/" bgColor="#000" fgColor="#fff" /></li>
+            </ul>
+        </div>
     </div>
 );
 
