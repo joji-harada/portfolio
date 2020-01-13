cf308f3aa304a43b01af1ad02cf06b885edbdb2c
George Allison <49288165+joji-harada@users.noreply.github.com>
2020-01-13 13:35:30 -0800

    switched to 3rd party api for form simplicity

--- src/components/Sections/Contact.js
+++ src/components/Sections/Contact.js
@@ -1,65 +1,76 @@
-import React from 'react';
-import './Contact.css';
+import React from "react";
+import "./Contact.css";
 
-class Contact extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            name: '',
-            comment: '',
-            errormessage: ''
-        };
-    }
-    mySubmitHandler = (event) => {
-        let nam = event.target.name;
-        let val = event.target.value;
-        let err = '';
-        event.preventDefault();
-        if (nam === "comment") {
-            if (val === '') {
-                err = <strong>You must enter a comment</strong>;
-            }
-        }
-        if(nam === "name"){
-            if (val === ''){
-                err = <strong>You must enter a name</strong>
-            }
-        }
-        this.setState({errormessage: err});
-        this.setState({[nam]: val});
-    }
-    render() {
-        return (
-            <div id="contact">
-                <form onSubmit={this.mySubmitHandler}>
-                    <h1>Contact Me</h1>
-                    <p>Enter your name:</p>
-                    <input
-                        type='text'
-                        name='username'
-                        style={{padding: '5px',
-                                fontWeight: 400,
-                                borderRadius: '5px'}}
-                    />
-                    {this.state.errormessage}
-                    <p>What service do you require?</p>
-                    <textarea
-                        type='text'
-                        name='comment'
-                        style={{resize: 'none',
-                               width: '250px',
-                               height: '100px',
-                               fontWeight: 400,
-                               borderRadius: '5px'}}
-                    >
-                    </textarea>
-                    {this.state.errormessage}
-                    <br/>
-                    <input type="submit"/>
-                </form>
-            </div>
-        );
-    }
+export default class MyForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.submitForm = this.submitForm.bind(this);
+    this.state = {
+      status: ""
+    };
+  }
+
+  render() {
+    const { status } = this.state;
+    return (
+      <div id="contact">
+        <form
+            onSubmit={this.submitForm}
+            action="https://formspree.io/meqaaaan"
+            method="POST"
+        >
+            <h2>SEND ME A MESSAGE!</h2>
+            <label style={{ margin: '10px 0' }}>Your Name:</label>
+                <input
+                type="text"
+                name="name"
+                style={{
+                    padding: '5px',
+                    fontWeight: 400,
+                    borderRadius: '5px'}}
+                />
+            <label style={{ margin: '10px 0' }}>Your Email:</label>
+                <input
+                type="email"
+                name="email"
+                style={{
+                    padding: '5px',
+                    fontWeight: 400,
+                    borderRadius: '5px'}}
+                />
+            <label style={{ margin: '10px 0' }}>Your Message:</label>
+                <textarea
+                type="text"
+                name="message"
+                style={{resize: 'none',
+                    width: '250px',
+                    height: '100px',
+                    fontWeight: 400,
+                    borderRadius: '5px'}}
+                />
+            {status === "SUCCESS" ? <p>Thanks!</p> : <button style={{ margin: '10px 0', fontWeight: 'bold' }}>Submit</button>}
+            {status === "ERROR" && <p>Ooops! There was an error.</p>}
+        </form>
+      </div>  
+    );
+  }
+
+  submitForm(ev) {
+    ev.preventDefault();
+    const form = ev.target;
+    const data = new FormData(form);
+    const xhr = new XMLHttpRequest();
+    xhr.open(form.method, form.action);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        form.reset();
+        this.setState({ status: "SUCCESS" });
+      } else {
+        this.setState({ status: "ERROR" });
+      }
+    };
+    xhr.send(data);
+  }
 }
-
-export default Contact;
