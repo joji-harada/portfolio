a7f37cc8184aba9497f3291eb258ef64eed76c99
George Allison <49288165+joji-harada@users.noreply.github.com>
2020-01-20 16:47:19 -0800

    slight restructure&style change.

--- src/components/Sections/Cards.js
+++ src/components/Sections/Cards.js
@@ -10,18 +10,18 @@ class Cards extends Component{
                 <div className="row">
                     <div className="col-md-4">
                         <Projects
-                            imgsrc={"https://media.giphy.com/media/LM8JT6zjBss57XpXMh/giphy.gif"}
-                            title="Java College DB"
-                            link="https://github.com/joji-harada/collegedb"
-                            description="A Java program designed to read in csv files, analyze the student data and assign the values to a student object then store all the student objects within a circular singly linked list. This data structure can then apply search, sort and filter methods to create sorted/filtered output files. All data structures and methods were custom created."
-                        />
-                    </div>
-                    <div className="col-md-4">
-                        <Projects
                             imgsrc={"https://media.giphy.com/media/cO9MAppND3IrZcI3ZM/giphy.gif"}
                             title="Top Pot Doughnuts Website"
                             link="https://github.com/joji-harada/toppot"
                             description="A full build website for Top Pot Doughnuts. I took design influence from local eateries and bars including Molly Moon's and Optimism Brewing Co. I utilized two JQuery libraries to implement several UX functions. The first was the parallax scrolling affect on each webpage, and more importantly I used an accordion in order to condense the locations list. PHP configure files were used to implement recaptcha form validation."
+                        />
+                    </div>
+                    <div className="col-md-4">
+                        <Projects
+                            imgsrc={"https://media.giphy.com/media/LM8JT6zjBss57XpXMh/giphy.gif"}
+                            title="Java College DB"
+                            link="https://github.com/joji-harada/collegedb"
+                            description="A Java program designed to read in csv files, analyze the student data and assign the values to a student object then store all the student objects within a circular singly linked list. This data structure can then apply search, sort and filter methods to create sorted/filtered output files. All data structures and methods were created from scratch."
                         />
                     </div>
                     <div className="col-md-4">
