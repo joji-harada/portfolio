19c0cfd2390bc76782471587379df994537bb2eb
George Allison <49288165+joji-harada@users.noreply.github.com>
2021-07-01 20:41:41 -0700

    updated resume link
    new resume linked in center content

--- src/components/CenterContent/CenterContent.js
+++ src/components/CenterContent/CenterContent.js
@@ -16,7 +16,7 @@ const CenterContent = props => (
             <ul className="centerList">
                 <li><SocialIcon url="https://github.com/joji-harada" title="GitHub" target="_blank" bgColor="#000" fgColor="#fff" /></li>
                 <li><SocialIcon url="https://www.linkedin.com/in/george-allison-728975139/" title="LinkedIn" target="_blank" bgColor="#000" fgColor="#fff" /></li>
-                <li><SocialIcon url="https://docs.google.com/document/d/10WK2DcJq4UghFlScD_5uJ4Kfs58mEcGT88mAZ6guzMA/edit?usp=sharing" title="Resume-Google Drive" target="_blank" bgColor="#000" fgColor="#fff" /></li>
+                <li><SocialIcon url="https://docs.google.com/document/d/1ZlK-f6k7gMvFi8Ylqcxm0oL_z5wVh7zK3UVw_dnb10Y/edit?usp=sharing" title="Resume-Google Drive" target="_blank" bgColor="#000" fgColor="#fff" /></li>
             </ul>
         </div>
     </div>
